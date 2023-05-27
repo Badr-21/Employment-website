82b78fd4d9a6a33ba6f78a8b3f7b11f422a03794
Badreddine <boudaoud.badreddine@gmail.com>
2023-05-27 22:07:46 +0200

    added home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Questions from "../components/Questions";
+import Testimonials from "../components/Testimonials";
+function Home() {
+   return (
+      <main>
+         <Header />
+         <Testimonials />
+         <Questions />
+         <Footer />
+      </main>
+   );
+}
+
+export default Home;
