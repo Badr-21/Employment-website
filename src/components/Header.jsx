b443180d00aa78f9546a0be6914e9713a470f28b
Badreddine <boudaoud.badreddine@gmail.com>
2023-06-08 21:36:15 +0200

    modified the header and make it responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,69 @@
+import { useState, useEffect, useRef } from "react";
 import { NavLink, Link } from "react-router-dom";
 import logo from "../assets/job-hunt-high-resolution-logo-color-on-transparent-background.svg";
+import closeIcon from "../assets/close-icon.svg";
+import menuHamburger from "../assets/menu-hamburger-icon.svg";
 
 function Header() {
+   const [showSideBar, setShowSideBar] = useState(false);
+   const [responsiveHeader, setResponsiveHeader] = useState(false);
+   const navRef = useRef();
+
+   const handleSideBar = () => {
+      setShowSideBar(!showSideBar);
+   };
+
+   useEffect(() => {
+      const handleResponsiveHeader = () => {
+         if (window.innerWidth < 768) {
+            setResponsiveHeader(true);
+         } else {
+            setResponsiveHeader(false);
+         }
+      };
+      handleResponsiveHeader();
+      window.addEventListener("resize", handleResponsiveHeader);
+      return () => window.removeEventListener("rezise", handleResponsiveHeader);
+   }, []);
+
+   useEffect(() => {
+      const closeSideBar = (e) => {
+         if (!navRef.current.contains(e.target)) {
+            setShowSideBar(false);
+         }
+      };
+      document.body.addEventListener("click", closeSideBar, true);
+      return () => document.body.removeEventListener("click", closeSideBar, true);
+   });
    return (
-      <header className="flex w-full items-center gap-x-16 px-8 py-4 text-[1.1rem] font-semibold shadow">
-         <Link to="/" reloadDocument>
+      <header className="relative z-10 flex w-full items-center gap-x-8 px-8 py-4 text-[1.1rem] font-semibold shadow md:gap-x-12">
+         <img
+            className={`${responsiveHeader ? "" : "hidden"} w-8 hover:cursor-pointer`}
+            src={menuHamburger}
+            alt="menu hamburger icon"
+            onClick={handleSideBar}
+         />
+         <Link className="flex-shrink-0" to="/" reloadDocument>
             <img className="w-36" src={logo} alt="logo" />
          </Link>
-         <nav className="flex w-full items-center justify-between">
-            <ul className="flex items-center gap-x-8 text-EerieBlack">
+         <nav
+            ref={navRef}
+            className={`${
+               showSideBar ? "left-[0rem]" : "left-[-12rem]"
+            } transition-left absolute top-0 flex w-[12rem] flex-col items-start gap-y-8 bg-white px-8 py-4 shadow duration-300 md:static md:w-full md:flex-row md:items-center md:justify-between md:shadow-none`}
+         >
+            <img
+               className={`${responsiveHeader ? "" : "hidden"} w-8 hover:cursor-pointer`}
+               src={closeIcon}
+               alt="close icon"
+               onClick={handleSideBar}
+            />
+            <ul className="flex flex-col gap-y-4 text-EerieBlack md:flex-row md:items-center md:gap-x-8">
                <NavLink
                   className={({ isActive }) => {
-                     return isActive ? "rounded-md bg-JungleGreenOne px-2 py-2 text-white" : {};
+                     return isActive
+                        ? "rounded-md bg-JungleGreenOne px-2 py-2 text-center text-white"
+                        : {};
                   }}
                   to="/"
                >
@@ -19,7 +71,9 @@ function Header() {
                </NavLink>
                <NavLink
                   className={({ isActive }) => {
-                     return isActive ? "rounded-md bg-JungleGreenOne px-2 py-2 text-white" : {};
+                     return isActive
+                        ? "rounded-md bg-JungleGreenOne px-2 py-2 text-center text-white"
+                        : {};
                   }}
                   to="search-job"
                >
@@ -27,16 +81,22 @@ function Header() {
                </NavLink>
                <NavLink
                   className={({ isActive }) => {
-                     return isActive ? "rounded-md bg-JungleGreenOne px-2 py-2 text-white" : {};
+                     return isActive
+                        ? "rounded-md bg-JungleGreenOne px-2 py-2 text-center text-white"
+                        : {};
                   }}
                   to="companies"
                >
                   <li>Companies</li>
                </NavLink>
             </ul>
-            <ul className="flex gap-x-8 text-JungleGreenTwo">
-               <li>Log In</li>
-               <li>Sing Up</li>
+            <ul className="flex flex-col gap-y-8 text-JungleGreenTwo md:flex-row md:gap-x-8">
+               <Link to="log-in">
+                  <li>Log In</li>
+               </Link>
+               <Link to="sign-up">
+                  <li>Sing Up</li>
+               </Link>
             </ul>
          </nav>
       </header>
