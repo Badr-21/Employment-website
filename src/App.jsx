2a98ffb16fc3290c2ae820384016611d6ab4dd3b
Badreddine <boudaoud.badreddine@gmail.com>
2023-07-07 22:52:08 +0200

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Header from "./components/Header";
 import AnimatedRoutes from "./animatedRoutes/AnimatedRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
